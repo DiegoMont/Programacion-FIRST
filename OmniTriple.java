d102947728fcb894d1a04578ffacbf694e656d38
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2018-07-02 16:58:29 -0500

    Update OmniTriple.java

--- OmniTriple.java
+++ OmniTriple.java
@@ -30,16 +30,31 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import org.firstinspires.ftc.robotcore.external.Drive;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="OmniTriple", group="Iterative Opmode")
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
 
-public class OmniTriple extends OpMode
+@TeleOp(name="RoboPro", group="Iterative Opmode")
+
+public class RoboPro extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -47,46 +62,59 @@ public class OmniTriple extends OpMode
     private DcMotor rightDrive = null;
     private DcMotor centreDrive = null;
     private DcMotor elevadorDrive = null;
-    private CRServo cajas = null;
-
-
-    //Code to run ONCE when the driver hits INIT
+    private CRServo cajasDrive = null;
+    double cajasPower = 0;
+    boolean presionar = false;
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
 
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
         leftDrive  = hardwareMap.get(DcMotor.class, "leftMotor");
         rightDrive = hardwareMap.get(DcMotor.class, "rightMotor");
         centreDrive = hardwareMap.get(DcMotor.class, "centreMotor");
         elevadorDrive = hardwareMap.get(DcMotor.class, "motor");
-        cajas = hardwareMap.get(CRServo.class, "cajas");
+        cajasDrive = hardwareMap.get(CRServo.class,"servoCajas");
 
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
-        centreDrive.setDirection(DcMotor.Direction.FORWARD);
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
+        // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
     }
 
-    //Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
     @Override
     public void init_loop() {
     }
 
-    //Code to run ONCE when the driver hits PLAY
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
     @Override
     public void start() {
         runtime.reset();
     }
 
-    //Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
     @Override
     public void loop() {
+        // Setup a variable for each drive wheel to save power level for telemetry
         double leftPower;
         double rightPower;
         double centrePower;
         double elevadorPower;
-        double cajasPower;
 
         // POV Mode uses left stick to go forward, and right stick to turn.
         double drive = -gamepad1.left_stick_y;
@@ -96,11 +124,11 @@ public class OmniTriple extends OpMode
         centrePower = gamepad1.right_stick_x;
 
         // Tank Mode uses one stick to control each wheel.
-        /*leftPower  = -gamepad1.left_stick_y ;
+        /*leftPower = -gamepad1.left_stick_y;
         rightPower = -gamepad1.left_stick_x ;*/
 
         // Control power of wheels.
-        if (!gamepad1.right_bumper) {
+        if (!gamepad1.a) {
           leftPower = leftPower * 0.75;
           rightPower = rightPower * 0.75;
           centrePower = centrePower * 0.75;
@@ -108,32 +136,42 @@ public class OmniTriple extends OpMode
 
         // Move the lift.
         if (gamepad1.dpad_up) {
-          centrePower = 0.5;
+          elevadorPower = 0.5;
         } else if (gamepad1.dpad_down) {
-          centrePower = -0.5
+          elevadorPower = -0.5;
         } else {
-          centrePower = 0;
+          elevadorPower = 0;
         }
 
         // Activate the movement of the mechanism to pick boxes.
-        boolean click;
-        if (gamepad1.left_bumper) {
-          click = true;
-        } else if (!gamepad1.left_bumper && click) {
-          if (cajasPower == 0) {
+        if (gamepad1.left_bumper || gamepad1.right_bumper) {
+          presionar = true;
+        } else if (!gamepad1.left_bumper && presionar) {
+          if (cajasPower == 0.0) {
+            cajasPower = -1.0;
+          } else {
+            cajasPower = 0.0;
+          }
+          presionar = false;
+        } else if (!gamepad1.right_bumper && presionar){
+          if (cajasPower == 0){
             cajasPower = 1;
           } else {
-            cajasPower == 0;
+            cajasPower = 0;
           }
-          click = false;
+          presionar = false;
+        } else {
+          presionar = false;
         }
 
         // Send calculated power to wheels
         leftDrive.setPower(leftPower);
         rightDrive.setPower(rightPower);
         centreDrive.setPower(centrePower);
+        cajasDrive.setPower(cajasPower);
         elevadorDrive.setPower(elevadorPower);
-        cajas.setPower(cajasPower);
+        
+        
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
@@ -141,7 +179,9 @@ public class OmniTriple extends OpMode
         telemetry.addData("SitckX", gamepad1.right_stick_x);
     }
 
-    //Code to run ONCE after the driver hits STOP
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
     @Override
     public void stop() {
     }
