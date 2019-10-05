a60562cef0d3ffefb28c10207b562009b58c4fbb
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2019-10-05 10:33:34 -0500

    Primeros programas de Hardlus

---
+++ FTC/2019/HardlusTeleoperado.java
@@ -0,0 +1,87 @@
+/*Copyright 2019
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Hardlus", group="Linear Opmode")
+public class TeleoperadoHardlus extends OpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private NaveDelOlvido hardbot = new NaveDelOlvido(this);
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        hardbot.getHardware(hardwareMap);
+
+        hardbot.resetEncoders();
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+      runtime.reset();
+    }
+
+    double desiredPosition;
+    @Override
+    public void loop() {
+      double frontLeftPower, frontRightPower, backLeftPower, backLeftPower, desviacion = hardbot.getDesviacion();
+
+      double drive = -gamepad1.left_stick_y;
+      double lateral = gamepad1.left_stick_x;
+      double turn = gamepad11.right_stick_x;
+      frontLeftPower = Range.clip(drive + lateral + turn, -1.0, 1.0);
+      frontRightPower = Range.clip(drive - lateral -turn, -1.0, 1.0);
+      backLeftPower = Range.clip(drive - lateral + turn, -1.0, 1.0);
+      backRightPower = Range.clip(drive + lateral - turn, -1.0, 1.0);
+
+      if(gamepad1.right_bumper){
+        leftPower *= 0.75;
+        rightPower *= 0.75;
+      }
+
+      if(gamepad1.left_bumper){
+        leftPower *= 0.5;
+        rightPower *= 0.5;
+      }
+
+      hardbot.frontLeft.setPower(frontLeftPower);
+      hardbot.frontRight.setPower(frontRightPower);
+      hardbot.backLeft.setPower(backLeftPower);
+      hardbot.backRight.setPower(backRightPower);
+
+      telemetry.addData("Status", "Run Time: " + runtime.toString());
+      telemetry.addData("Desviacion:", desviacion);
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
---
+++ FTC/2019/NaveDelOlvido.java
@@ -0,0 +1,119 @@
+/*Copyright 2019
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+@Disabled
+public class NaveDelOlvido {
+
+  //Inicializar variables para motores y sensores del robot
+  public DcMotor frontLeft = null;
+  public DcMotor frontRight = null;
+  public DcMotor backLeft = null;
+  public DcMotor backRight = null;
+
+  private LinearOpMode programa;
+  private BNO055IMU imu;
+  private Orientation angles;
+  private Acceleration gravity;
+
+  public NaveDelOlvido(LinearOpMode programa){
+    this.programa = programa;
+  }
+
+  //Metodo para buscar motores y servomotores del Expansion y asignarlos a las variables
+  public void getHardware(HardwareMap hwMap){
+      frontLeft = hwMap.get(DcMotor.class, "front_left_motor");
+      frontRight = hwMap.get(DcMotor.class, "front_right_motor");
+      backLeft = hwMap.get(DcMotor.class, "back_left_motor");
+      backRight = hwMap.get(DcMotor.class, "back_right_motor");
+
+      frontLeft.setDirection(DcMotor.Direction.FORWARD);
+      frontRight.setDirection(DcMotor.Direction.FORWARD);
+      backLeft.setDirection(DcMotor.Direction.FORWARD);
+      backRight.setDirection(DcMotor.Direction.FORWARD);
+
+      BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+      parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+      parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+      parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+      parameters.loggingEnabled      = true;
+      parameters.loggingTag          = "IMU";
+      parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+      imu = hwMap.get(BNO055IMU.class, "imu");
+      imu.initialize(parameters);
+      imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+  }
+
+  public void frenar(){
+    frontLeft.setPower(0);
+    frontRight.setPower(0);
+    backLeft.setPower(0);
+    backRight.setPower(0);
+  }
+
+  public void resetEncoders(){
+    frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    defaultRunmode();
+  }
+
+  public void defaultRunmode(){
+    frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+  }
+
+  public double getDesviacion(){
+    angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+    gravity  = imu.getGravity();
+    return angles.firstAngle;
+  }
+
+  /* Metodos para programacion autonoma */
+
+  //Este metodo movera al robot en linea recta la distancia que se especifique
+  public void moverDistanciaRecta(double distancia){
+    if(!programa.opModeIsActive()) return;
+
+    defaultRunmode();
+  }
+
+  public void setGiroDeNoventaGrados(String direction){
+
+  }
+
+}
