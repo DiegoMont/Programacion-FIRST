450ecf295ca7cc208be80720eec4f38d523f74b9
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2020-06-12 20:56:10 -0500

    Login y menu navegacion funcional

--- Scouting-Nautilus/Proyecto/index.php
+++ Scouting-Nautilus/Proyecto/index.php
@@ -1,3 +1,41 @@
+<?php
+//Constante para contraseña
+define('PASSWORD', "futuroswinners");
+define('KEY_SESION', "inicializada");
+
+session_start(['cookie_lifetime' => 86400]);
+
+//Mensaje de error
+$mensaje = "";
+
+//Si ya hay una sesion iniciada, redirigir a menu
+if (isset($_SESSION["sesion"])) {
+  if ($_SESSION["sesion"] === KEY_SESION) {
+    header("Location:menu.php");
+    exit;
+  }
+}
+
+//Si ya dieron click en el boton de submit
+if (isset($_POST["btn-submit"])) {
+
+  //Checar si enviaron la contraseña
+  if (isset($_POST["contrasena"])) {
+    if ($_POST["contrasena"] === PASSWORD) {
+      //Si la contraseña es correcta iniciar sesion y redirigir|
+      $_SESSION["sesion"] = KEY_SESION;
+      header("Location:menu.php");
+      exit;
+    } else {
+      //Si la contraseña es incorrecta mandar error
+      $mensaje = "<p class='error'>La contrase&ntilde;a es incorrecta</p>";
+    }
+  } else {
+    //Si el input esta vacio mandar error
+    $mensaje = "<p class='error'>Ingresa una contrase&ntilde;a</p>";
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
   <head>
@@ -9,6 +47,7 @@
     <link rel="stylesheet" type="text/css" href="css/general.css">
     <link rel="stylesheet" type="text/css" href="css/estilo-index.css">
     <link rel="icon" type="image/png" href="img/nautilus.png">
+    <script src="js/seguridad-index.js" charset="utf-8" defer></script>
   </head>
   <body>
     <header>
@@ -20,10 +59,12 @@
     <section>
       <div class="login">
         <h1 class="login">SIGN IN</h1>
-        <form action="" method="POST">
+        <form action="index.php" method="POST" onsubmit="return verificar()">
           <label for="pass" class="login">Contrase&ntilde;a:</label>
           <input type="password" name="contrasena" id="pass" placeholder="Contrase&ntilde;a" class="login">
-          <button type="submit" name="submit" id="btn-submit" value="Log in">Log in</button>
+          <p class="error ocultar" id="error-pass"></p>
+          <?php echo $mensaje; ?>
+          <button type="submit" name="btn-submit" id="btn-submit" value="Log in">Log in</button>
         </form>
       </div>
     </section>
