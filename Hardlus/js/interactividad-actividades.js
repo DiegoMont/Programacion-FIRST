eb76123a8ec302018e3e571e38b25155a69079ea
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2020-08-31 02:51:47 -0500

    Pagina se va a repo nuevo

--- Hardlus/js/interactividad-actividades.js
+++
@@ -1,8 +0,0 @@
-function contarActividades() {
-  const arregloActividades = document.getElementsByClassName("actividad");
-  const parrafoCuenta = document.getElementById("cuenta-act");
-  let n = arregloActividades.length;
-  parrafoCuenta.innerText = (n == 1 ? "1 actividad": n + " actividades");
-}
-
-contarActividades();
