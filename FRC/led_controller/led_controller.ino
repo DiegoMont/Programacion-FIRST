a3ce96ab4329a7a6eb2f4a0ce0abe25c31d2d44b
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-03-20 13:20:57 -0600

    Compilation problems

--- FRC/led_controller/led_controller.ino
+++ FRC/led_controller/led_controller.ino
@@ -1,15 +1,64 @@
-#include "controller.cpp"
+const int RED_PIN = 9;
+const int GREEN_PIN = 10;
+const int BLUE_PIN =11;
 
-const int RED_PIN = 5;
-const int GREEN_PIN = 6;
-const int BLUE_PIN = 9;
+class LedController {
+    private:
+    int redPin;
+    int greenPin;
+    int bluePin;
+    
+    public:
+    LedController(int redPin, int greenPin, int bluePin){
+        this->redPin = redPin;
+        this->greenPin = greenPin;
+        this->bluePin = bluePin;
+    }
+
+    void initializePins(){
+        pinMode (redPin, OUTPUT);
+        pinMode (greenPin, OUTPUT);
+        pinMode (bluePin, OUTPUT);
+        turnOff();
+        
+    }
+
+    void testLEDs() {
+        // TODO: Implement
+        const int DELAY_MS = 200;
+        for(int i = 0; i < 3; i++) {
+            int color = 0xff0000 >> (8*i);
+            setColor(color);
+            delay(delay);
+            turnOff();
+            delay(DELAY_MS);
+        }
+    }
+
+    void setColor(int hexColor) {
+        int red = (hexColor >> 16) & 255;
+        int green = (hexColor >> 8) & 255;
+        int blue = hexColor & 255;
+        setColor(red, green, blue);
+    }
+
+    void setColor(int red, int green, int blue) {
+        analogWrite(redPin, red);
+        analogWrite(greenPin, green);
+        analogWrite(bluePin, blue);
+    }
+
+    void turnOff() {
+        setColor(0x0);
+    }
+};
 
 LedController controller(RED_PIN, GREEN_PIN, BLUE_PIN);
 
 void setup() {
     controller.initializePins();
-    controller.testLEDs();
 }
 
 void loop() {
+    controller.testLEDs();
 }
