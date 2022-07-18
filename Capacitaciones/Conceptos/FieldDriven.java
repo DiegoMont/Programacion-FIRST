fee180c3debcec481b40d2fca943a3124238162e
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-07-18 14:03:08 -0500

    Field oriented

--- Capacitaciones/Conceptos/FieldDriven.java
+++ Capacitaciones/Conceptos/FieldDriven.java
@@ -1,55 +1,60 @@
-public class FieldDriven extends LinearOpMode {
-    private BNO055IMU imu;
-    private boolean fieldOriented = false;
-    private double FIELD_NORTH;
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+
+
+@Autonomous(name = "Field oriented drive")
+public class FieldOriented extends LinearOpMode {
+    RobotAna robot = new RobotAna();
+    boolean fieldOriented = false;
+    double FIELD_NORTH;
+    
     @Override
     public void runOpMode() {
-        initIMU();
+        robot.initHardware(hardwareMap);
+        robot.iniciarAcelerometro(hardwareMap, telemetry);
         waitForStart();
-        FIELD_NORTH = getHeading();
+        double FIELD_NORTH = robot.getHeading();
         boolean drivingModeClick = false;
         while(opModeIsActive()) {
-            if(!gamepad1.dpad_down && drivingModeClick) {
+            if(!gamepad1.dpad_down && drivingModeClick)
                 fieldOriented = !fieldOriented;
-                fieldNorth = getHeading();
-            }            
             drivingModeClick = gamepad1.dpad_down;
-            controlChasis();
+            controlChasis(fieldOriented);
+            telemetry.update();
         }
     }
-
-    private void controlChasis() {
+    
+    private void controlChasis(boolean fieldOriented) {
         double drive = -gamepad1.left_stick_y;
         double lateral = gamepad1.left_stick_x;
         double turn = gamepad1.right_stick_x;
+        telemetry.addData("Drive mode:", fieldOriented ? "Field orientd": "Robot oriented");
         if(fieldOriented) {
-            double heading = getHeading();
+            double heading = robot.getHeading();
             double degreesToRotate = FIELD_NORTH - heading;
-            lateral = Math.cos(lateral * Math.toRadians(degreesToRotate)) - Math.sin(drive * Math.toRadians(degreesToRotate));
-            drive = Math.sin(lateral * Math.toRadians(degreesToRotate)) + Math.cos(drive * Math.toRadians(degreesToRotate));
+            lateral = lateral * Math.cos(Math.toRadians(degreesToRotate)) - drive * Math.sin(Math.toRadians(degreesToRotate));
+            drive = lateral * Math.sin(Math.toRadians(degreesToRotate)) + drive * Math.cos(Math.toRadians(degreesToRotate));
+            telemetry.addData("North:", FIELD_NORTH);
+            telemetry.addData("Giro:", heading);
+            telemetry.addData("Lateral:", lateral);
+            telemetry.addData("Drive:", drive);
         }
-        // TODO: Assign motor power
+        setChasisPower(drive, lateral, turn);
     }
-
-    public void initIMU() {
-        telemetry.addData("Calibrando ", "IMU");
-        telemetry.update();
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
-        parameters.loggingEnabled      = true;
-        parameters.loggingTag          = "IMU";
-        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-        imu.initialize(parameters);
-        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+    
+    public void setChasisPower(double drive, double lateral, double turn) {
+        double frPower = drive - lateral - turn;
+        double flPower = drive + lateral + turn;
+        double blPower = drive - lateral + turn;
+        double brPower = drive + lateral - turn;
+        double powerMultiplier = 0.25;
+        robot.FR.setPower(frPower * powerMultiplier);
+        robot.FL.setPower(flPower * powerMultiplier);
+        robot.BR.setPower(brPower * powerMultiplier);
+        robot.BL.setPower(blPower * powerMultiplier);
     }
-
-    public double getHeading() {
-        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        return angles.firstAngle;
-    }
+    
 }
