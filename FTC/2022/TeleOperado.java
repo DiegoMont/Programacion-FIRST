a6a91438d57025e676dcd8f221e6134a1fe63d6e
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-10-12 00:16:52 -0500

    Implemented motor ramp

--- FTC/2022/TeleOperado.java
+++ FTC/2022/TeleOperado.java
@@ -7,7 +7,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="TeleOp")
-public class Teleoperado extends LinearOpMode {
+public class TeleOperado extends LinearOpMode {
+    final double UPDATE_PERIOD_MS = 300;
+    final double MOTOR_POWER_INCREMENT = 0.02;
+
     DcMotor frontLeft, frontRight, backLeft, backRight;
 
     @Override
@@ -18,30 +21,48 @@ public class Teleoperado extends LinearOpMode {
         backRight = hardwareMap.get(DcMotor.class, "back_right");  
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.REVERSE);
+        timer = new ElapsedTime();
+        double lastUpdateTime = timer.milliseconds();
         
         waitForStart();
         while(opModeIsActive()) {
-            double drive = -gamepad1.left_stick_y;
-            double lateral = gamepad1.right_stick_x;
-            double turn = gamepad1.left_stick_x;
-            double powerMultiplier = getPowerMultiplier(gamepad1);
-            double frontLeftPower = Range.clip(drive + lateral + turn, -1, 1) * powerMultiplier;
-            double frontRightPower = Range.clip(drive - lateral - turn, -1, 1) * powerMultiplier;
-            double backLeftPower = Range.clip(drive - lateral + turn, -1, 1) * powerMultiplier;
-            double backRightPower = Range.clip(drive + lateral - turn, -1, 1) * powerMultiplier;
-            frontLeft.setPower(frontLeftPower);
-            frontRight.setPower(frontRightPower);
-            backLeft.setPower(backLeftPower);
-            backRight.setPower(backRightPower);
+            double currentTime = timer.milliseconds();
+            if (currentTime - lastUpdateTime > UPDATE_PERIOD){
+                double drive = -gamepad1.left_stick_y;
+                double lateral = gamepad1.right_stick_x;
+                double turn = gamepad1.left_stick_x;
+                double powerMultiplier = getPowerMultiplier(gamepad1.right_bumper, gamepad1.left_bumper);
+                double frontLeftTarget = Range.clip(drive + lateral + turn, -1, 1) * powerMultiplier;
+                double frontRightTarget = Range.clip(drive - lateral - turn, -1, 1) * powerMultiplier;
+                double backLeftTarget = Range.clip(drive - lateral + turn, -1, 1) * powerMultiplier;
+                double backRightTarget = Range.clip(drive + lateral - turn, -1, 1) * powerMultiplier;
+                double frontLeftPower = getIncreasedPower(frontLeft.getPower(), frontLeftTarget);
+                double frontRightPower = getIncreasedPower(frontRight.getPower(), frontRightTarget);
+                double backLeftPower = getIncreasedPower(backLeft.getPower(), backLeftTarget);
+                double backRightPower = getIncreasedPower(backRight.getPower(), backRightTarget);
+                frontLeft.setPower(frontLeftPower);
+                frontRight.setPower(frontRightPower);
+                backLeft.setPower(backLeftPower);
+                backRight.setPower(backRightPower);
+                lastUpdateTime = currentTime;
+            }
             
             telemetry.update();
         }
     }
 
-    private double getPowerMultiplier(Gamepad gamepad){
-        if(gamepad.right_bumper)
+    double getIncreasedPower(double current, double target) {
+        double diff = target - current;
+        double absDiff = Math.abs(diff);
+        double sign = diff / absDiff;
+        double valueToIncrement = sign * Math.min(absDiff, MOTOR_POWER_INCREMENT);
+        return current + valueToIncrement;
+    }
+
+    double getPowerMultiplier(boolean accelerateBtn, boolean breakBtn){
+        if(accelerateBtn)
             return 1;
-        if(gamepad.left_bumper)
+        if(breakBtn)
             return 0.5;
         return 0.8;
     }
